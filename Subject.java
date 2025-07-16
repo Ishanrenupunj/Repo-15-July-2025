3ff4072f1c25cec2255ff48f8993adeaa8f45c2b
Ishan <punjishanrenu@gmail.com>
2025-07-16 12:13:40 +0800

    added property methods

--- Subject.java
+++ Subject.java
@@ -7,7 +7,7 @@ public class Subject {
     public String getName(){
         return name;
     }
-    public  void setName(name){
+    public  void setName(String name){
         this.name=name;
         
         
@@ -15,10 +15,28 @@ public class Subject {
     public String getSubID(){
         return SubID;
     }
-    public void setSubID(SubID){
+    public void setSubID(String SubID){
         this.SubID=SubID;
 
     }
-    public String getTeacher
+    public String getTeacherName(){
+        return teacherName;
+    }
+    public void setTeacherName(String teacherName){
+        this.teacherName=teacherName;
+    }
+    public int getMaxMarks(){
+        return MaxMarks;
+
+    }
+    public void setMaxMarks(int MaxMarks){
+        this.MaxMarks=MaxMarks;
+    }
+    public int getMarksObtained(){
+        return MarksObtained;
+    }
+    public void setMarksObtained(int MarksObtained){
+        this.MarksObtained=MarksObtained;
+    }
     
 }
