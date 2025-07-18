1d034b4b4eeada08e583353d6baee9c9156d1bca
Ishan <punjishanrenu@gmail.com>
2025-07-18 12:48:11 +0800

    added property methods

--- Student.java
+++ Student.java
@@ -3,5 +3,23 @@ public class Student {
     private int rollNo;
     private String subject;
     private String department;
-    
+    public String getName(){
+        return name;
+    }
+    public void setDepartment(String department){
+        this.department=department;
+    }
+    public String getDepartment(){
+        return department;
+    }
+    public int getRollNo(){
+        return rollNo;
+    }
+    public void setSubject(int rollNo){
+        this.rollNo=rollNo;
+
+    }
+    public String getSubject(){
+        return subject;
+    }
 }
